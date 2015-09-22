1c27eb988501021905faee83d585173063b194fd
Gustav <sir.gustav.the.coder@gmail.com>
2015-09-22 14:00:40 +0200

    initial commit

---
+++ PostTestr/CustomCommands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostTestr
+{
+    using System.Windows.Input;
+
+    public static class CustomCommands
+    {
+        public static RoutedCommand Exit = new RoutedCommand();
+        public static RoutedCommand Execute = new RoutedCommand();
+        public static RoutedCommand Login = new RoutedCommand();
+        public static RoutedCommand Format = new RoutedCommand();
+    }
+}
