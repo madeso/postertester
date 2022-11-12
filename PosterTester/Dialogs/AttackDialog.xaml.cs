5df257e7de1e519e3c9214c41a5d3b28803cacb7
Gustav <sir.gustav.the.coder@gmail.com>
2022-11-12 12:30:04 +0100

    style: dialog source cleanups

--- PosterTester/Dialogs/AttackDialog.xaml.cs
+++ PosterTester/Dialogs/AttackDialog.xaml.cs
@@ -1,44 +1,33 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using PosterTester.Data;
 
-namespace PosterTester
+namespace PosterTester;
+
+/// <summary>
+/// Interaction logic for AttackDialog.xaml
+/// </summary>
+public partial class AttackDialog : Window
 {
-	/// <summary>
-	/// Interaction logic for AttackDialog.xaml
-	/// </summary>
-	public partial class AttackDialog : Window
+	public AttackDialog(AttackOptions attackOptions)
 	{
-		public AttackDialog(AttackOptions attackOptions)
-		{
-			InitializeComponent();
-			this.AttackOptions = attackOptions;
-			this.DataContext = attackOptions;
-		}
+		InitializeComponent();
+		this.AttackOptions = attackOptions;
+		this.DataContext = attackOptions;
+	}
 
-		public AttackOptions AttackOptions { get; }
+	public AttackOptions AttackOptions { get; }
 
-		private void Window_Activated(object sender, EventArgs e)
-		{
-			this.dlgCount.SelectAll();
-			this.dlgCount.Focus();
-		}
+	private void Window_Activated(object sender, EventArgs e)
+	{
+		this.dlgCount.SelectAll();
+		this.dlgCount.Focus();
+	}
 
-		private void OkExecuted(object sender, ExecutedRoutedEventArgs e)
-		{
-			this.DialogResult = true;
-			Close();
-		}
+	private void OkExecuted(object sender, ExecutedRoutedEventArgs e)
+	{
+		this.DialogResult = true;
+		Close();
 	}
 }
--- PosterTester/Dialogs/PlotDisplay.xaml.cs
+++ PosterTester/Dialogs/PlotDisplay.xaml.cs
@@ -1,29 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace PosterTester
+namespace PosterTester;
+
+/// <summary>
+/// Interaction logic for PlotDisplay.xaml
+/// </summary>
+public partial class PlotDisplay : Window
 {
-	/// <summary>
-	/// Interaction logic for PlotDisplay.xaml
-	/// </summary>
-	public partial class PlotDisplay : Window
+	public PlotDisplay(Action<ScottPlot.Plot> plotFunction)
 	{
-		public PlotDisplay(Action<ScottPlot.Plot> plotFunction)
-		{
-			InitializeComponent();
-			plotFunction(dlgPlot.Plot);
-			dlgPlot.Refresh();
-		}
+		InitializeComponent();
+		plotFunction(dlgPlot.Plot);
+		dlgPlot.Refresh();
 	}
 }
