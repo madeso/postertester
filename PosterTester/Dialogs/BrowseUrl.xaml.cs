c6cbd140d887896f6341664b39cc674a3cc8b3ab
Gustav <sir.gustav.the.coder@gmail.com>
2024-09-19 09:35:13 +0200

    Make url browse a little bit better

--- PosterTester/Dialogs/BrowseUrl.xaml.cs
+++ PosterTester/Dialogs/BrowseUrl.xaml.cs
@@ -26,8 +26,23 @@ public partial class BrowseUrl : Window
 
 public class UrlData : INotifyPropertyChanged
 {
-
-	public event PropertyChangedEventHandler PropertyChanged;
+	private string _absoluteUri;
+	private string _authority;
+	private string _dnsSafeHost;
+	private string _fragment;
+	private string _host;
+	private string _idnHost;
+	private string _scheme;
+	private string _userInfo;
+	private int _port;
+	private bool _isDefaultPort;
+	private bool _isAbsoluteUri;
+	private bool _isFile;
+	private bool _isLoopback;
+	private bool _isUnc;
+	private bool _userEscaped;
+	private string[] _segments;
+	public event PropertyChangedEventHandler? PropertyChanged;
 
 	public UrlData(string url)
 	{
@@ -37,7 +52,6 @@ public class UrlData : INotifyPropertyChanged
 
 	private void FromUrl(Uri u)
 	{
-		this.AbsolutePath = u.AbsolutePath;
 		this.AbsoluteUri = u.AbsoluteUri;
 		this.Authority = u.Authority;
 		this.DnsSafeHost = u.DnsSafeHost;
@@ -56,30 +70,129 @@ public class UrlData : INotifyPropertyChanged
 		this.UserInfo = u.UserInfo;
 	}
 
-	public string AbsolutePath { get; set; }
-	public string AbsoluteUri { get; set; }
-	public string Authority { get; set; }
-	public string DnsSafeHost { get; set; }
-	public string Fragment { get; set; }
-	public string Host { get; set; }
-	public string IdnHost { get; set; }
-	public bool IsDefaultPort { get; set; }
-	public bool IsAbsoluteUri { get; set; }
-	public bool IsFile { get; set; }
-	public bool IsLoopback { get; set; }
-	public bool IsUnc { get; set; }
-	public int Port { get; set; }
-	public string Scheme { get; set; }
-	public string[] Segments { get; set; }
-	public bool UserEscaped { get; set; }
-	public string UserInfo { get; set; }
+	private static Uri? ParseUri(string u)
+	{
+		if (Uri.TryCreate(u, UriKind.RelativeOrAbsolute, out var uri))
+		{
+			return uri;
+		}
+		else
+		{
+			return null;
+		}
+	}
 
-	protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	public string AbsoluteUri
+	{
+		get => this._absoluteUri;
+		set
+		{
+			if (SetField(ref this._absoluteUri, value))
+			{
+				var u = ParseUri(this._absoluteUri);
+				if (u == null) return;
+				FromUrl(u);
+			}
+		}
+	}
+
+	public string Authority
+	{
+		get => this._authority;
+		set => SetField(ref this._authority, value);
+	}
+
+	public string DnsSafeHost
+	{
+		get => this._dnsSafeHost;
+		set => SetField(ref this._dnsSafeHost, value);
+	}
+
+	public string Fragment
+	{
+		get => this._fragment;
+		set => SetField(ref this._fragment, value);
+	}
+
+	public string Host
+	{
+		get => this._host;
+		set => SetField(ref this._host, value);
+	}
+
+	public string IdnHost
+	{
+		get => this._idnHost;
+		set => SetField(ref this._idnHost, value);
+	}
+
+	public string Scheme
+	{
+		get => this._scheme;
+		set => SetField(ref this._scheme, value);
+	}
+
+	public string UserInfo
+	{
+		get => this._userInfo;
+		set => SetField(ref this._userInfo, value);
+	}
+
+	public int Port
+	{
+		get => this._port;
+		set => SetField(ref this._port, value);
+	}
+
+	public bool IsDefaultPort
+	{
+		get => this._isDefaultPort;
+		set => SetField(ref this._isDefaultPort, value);
+	}
+
+	public bool IsAbsoluteUri
+	{
+		get => this._isAbsoluteUri;
+		set => SetField(ref this._isAbsoluteUri, value);
+	}
+
+	public bool IsFile
+	{
+		get => this._isFile;
+		set => SetField(ref this._isFile, value);
+	}
+
+	public bool IsLoopback
+	{
+		get => this._isLoopback;
+		set => SetField(ref this._isLoopback, value);
+	}
+
+	public bool IsUnc
+	{
+		get => this._isUnc;
+		set => SetField(ref this._isUnc, value);
+	}
+
+	public bool UserEscaped
+	{
+		get => this._userEscaped;
+		set => SetField(ref this._userEscaped, value);
+	}
+
+	public string[] Segments
+	{
+		get => this._segments;
+		set => SetField(ref this._segments, value);
+	}
+
+
+	protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
 	{
 		this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 
-	protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+	protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
 	{
 		if (EqualityComparer<T>.Default.Equals(field, value)) return false;
 		field = value;
