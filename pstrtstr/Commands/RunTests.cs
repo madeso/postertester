d85c926e797cbf8a894928dfc8d8b3ca73a54fe9
Gustav <sir.gustav.the.coder@gmail.com>
2025-10-21 12:21:13 +0200

    Add report when running tests in commandline

--- pstrtstr/Commands/RunTests.cs
+++ pstrtstr/Commands/RunTests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using PosterLib.Data;
 using PosterLib.Domain;
@@ -11,13 +12,21 @@ namespace pstrtstr.Commands;
 public class RunTestsSettings : CommandSettings
 {
 	[CommandArgument(0, "[GROUP]")]
+	[Description("The group/service file (*.PosterTesterGroup)")]
 	public string GroupFile { get; set; } = string.Empty;
 
+	// todo(Gustav): remove this in favor of passing the bearer token directly
 	[CommandOption("-a|--authfile <AUTHFILE>")]
+	[Description("The auth file to use for requests that require authentication")]
 	public string AuthFile { get; set; } = string.Empty;
 
 	[CommandOption("-b|--base-url <URL>")]
+	[Description("The base url to use for requests that use the base url")]
 	public string? BaseUrl { get; set; } = null;
+
+	[CommandOption("-p|--passed")]
+	[Description("Write passed tests to the report")]
+	public bool WritePassedTests { get; set; } = false;
 }
 
 
@@ -31,6 +40,10 @@ public class RunTestsCommand : AsyncCommand<RunTestsSettings>
 	private static async Task<int> run(CommandContext context, RunTestsSettings settings)
 	{
 		int errorCount = 0;
+
+		var okTests = new List<string>();
+		var failedTests = new List<string>();
+
 		try
 		{
 			var loaded = Disk.LoadSharedGroupFile(settings.GroupFile);
@@ -63,29 +76,52 @@ public class RunTestsCommand : AsyncCommand<RunTestsSettings>
 					using var cancel = new CancellationTokenSource();
 					foreach (var req in loaded.Requests)
 					{
-						AnsiConsole.MarkupLineInterpolated($"Running [blue]{req.TitleOrUrl}[/]");
+						var testName = req.TitleOrUrl;
+						AnsiConsole.MarkupLineInterpolated($"Running [blue]{testName}[/]");
 						var response = await Logic.SingleAttack(group, req, auth, cancel.Token);
-						SetSpinnerStatus(response, ctx, ref errorCount);
-
+						
 						if (response.Error != null)
 						{
+							errorCount += 1;
+							ctx.Status(CreateStatus(errorCount));
+							ctx.SpinnerStyle(Style.Parse("red"));
+						
 							AnsiConsole.MarkupLine("Test [red]FAILED[/]");
 							AnsiConsole.MarkupInterpolated($"[red]{response.Error}[/]");
+							failedTests.Add(testName);
 						}
 						else
 						{
 							var timeTaken = Logic.TimeSpanToString(response.Result);
 							AnsiConsole.MarkupLine("Test [green]OK[/]");
 							AnsiConsole.MarkupInterpolated($"Took [blue]{timeTaken}[/]");
+							okTests.Add(testName);
 						}
 					}
 				});
 
-			if(errorCount > 0)
+			// Print report
+			AnsiConsole.WriteLine();
+			AnsiConsole.MarkupLine("[underline]Test Report[/]");
+			foreach (string test in failedTests)
+			{
+				AnsiConsole.MarkupLineInterpolated($"* [red]{test}[/]");
+			}
+			AnsiConsole.MarkupLineInterpolated($"[red]{failedTests.Count}[/] tests failed.");
+			if(settings.WritePassedTests)
+			{
+				foreach (string test in okTests)
+				{
+					AnsiConsole.MarkupLineInterpolated($"* [green]{test}[/]");
+				}
+			}
+			AnsiConsole.MarkupLineInterpolated($"[green]{okTests.Count}[/] tests passed.");
+
+			if (errorCount > 0)
 			{
 				int totalTests = loaded.Requests.Count;
 
-				AnsiConsole.MarkupInterpolated($"Error: [red]{errorCount}[/]/{totalTests} tests [red]FAILED[/]");
+				AnsiConsole.MarkupLineInterpolated($"Error: [red]{errorCount}[/]/{totalTests} tests [red]FAILED[/]");
 				return -errorCount;
 			}
 			else
